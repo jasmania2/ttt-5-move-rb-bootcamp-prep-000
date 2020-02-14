d6732157ca3db067823fd378fb76811ab140a213
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-14 23:33:00 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,16 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(number)
+  position = number.to_i - 1
+  return position
+end
+
+def move (board, index, character = "X")
+  board = [" "," "," "," "," "," "," "," "," "]
+  def update_array_at_with(array, index, value)
+  array[index] = value
+end
+  index = #{board[input_to_index(number)]}
+  character = "X"||"O"
+end
